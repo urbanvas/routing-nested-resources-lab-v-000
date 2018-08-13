31ac2deebb153fb099001a1674e483e60a1a6e7f
Neil Seejoor <nvseejoor@gmail.com>
2018-08-13 23:27:40 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,52 +1,53 @@
-class SongsController < ApplicationController
+
+class ArtistsController < ApplicationController
   def index
-    @songs = Song.all
+    @artists = Artist.all
   end
 
   def show
-    @song = Song.find(params[:id])
+    @artist = Artist.find(params[:id])
   end
 
   def new
-    @song = Song.new
+    @artist = Artist.new
   end
 
   def create
-    @song = Song.new(song_params)
+    @artist = Artist.new(artist_params)
 
-    if @song.save
-      redirect_to @song
+    if @artist.save
+      redirect_to @artist
     else
       render :new
     end
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @artist = Artist.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
+    @artist = Artist.find(params[:id])
 
-    @song.update(song_params)
+    @artist.update(artist_params)
 
-    if @song.save
-      redirect_to @song
+    if @artist.save
+      redirect_to @artist
     else
       render :edit
     end
   end
 
   def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    flash[:notice] = "Artist deleted."
+    redirect_to artists_path
   end
 
   private
 
-  def song_params
-    params.require(:song).permit(:title, :artist_name)
+  def artist_params
+    params.require(:artist).permit(:name)
   end
 end
